6f45fa8b589e57a18c7c673db6bb19c3d8c65891
unknown <gelanlucas@gmail.com>
2023-03-06 03:03:28 -0300

    feat: finish

--- users/models.py
+++ users/models.py
@@ -3,5 +3,12 @@ from django.contrib.auth.models import AbstractUser
 
 
 class User(AbstractUser):
+    email = models.EmailField(
+        max_length=127, 
+        unique=True, 
+        error_messages={"unique": "This field must be unique."}
+    )
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
+    birthdate = models.DateField(null=True)
+    is_employee = models.BooleanField(null=True, default=False)
